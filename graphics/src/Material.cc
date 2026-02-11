fcce15e7c4bdb3916bc18f18277e7ee3db691311
Addisu Z. Taddese <addisu@openrobotics.org>
2026-02-11 16:44:18 -0600

    Fix error to resolve texture file in OBJ loader
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- graphics/src/Material.cc
+++ graphics/src/Material.cc
@@ -164,17 +164,17 @@ void Material::SetTextureImage(const std::string &_tex,
   // If the texture image doesn't exist then try the next most likely path.
   if (!exists(this->dataPtr->texImage))
   {
-    // Try to resolve this texture image to a locally cached path in a
-    // separate directory
-    this->dataPtr->texImage = common::findFile(_tex);
+    this->dataPtr->texImage = common::joinPaths(_resourcePath, "..",
+                                                "materials", "textures", _tex);
     if (!exists(this->dataPtr->texImage))
     {
-      this->dataPtr->texImage = common::joinPaths(_resourcePath, "..",
-          "materials", "textures", _tex);
+      // Try to resolve this texture image to a locally cached path in a
+      // separate directory
+      this->dataPtr->texImage = common::findFile(_tex);
       if (!exists(this->dataPtr->texImage))
       {
         gzerr << "Unable to find texture [" << _tex << "] as a locally"
-              " cached texture or in path ["<< _resourcePath << "]\n";
+          " cached texture or in path ["<< _resourcePath << "]\n";
       }
     }
   }
